8ef25730553ca3b76a039cdb92899a1f208b749b
ziegnes <ziegnes@example.com>
2023-11-01 16:55:17 -0700

    trying out the shared package route

---
+++ models.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from typing import Any, List, Optional
+
+# data object for python test execution
+class SingleTestRunResult(BaseModel):
+    test_pass: bool
+    error_message: Optional[str] = None
+    input: Any
+    actual_output: Any
+    expected_output: Any
+
+class TestRunnerResult(BaseModel):
+    session_id: str
+    test_outputs: List[SingleTestRunResult]
+    num_total_tests: int
+    num_tests_passed: int
+
+# data class between autograder and testrunner
+class JobError(BaseModel):
+    error_type: str
+    message: str
+
+class JobResult(BaseModel):
+    session_id: str
+    error: Optional[JobError] = None
+    test_results: Optional[TestRunnerResult] = None
+
+
+# data class for the code grader
+class GradeCodeError(BaseModel):
+    error_type: str
+    message: str
+
+class GradeCodeResult(BaseModel):
+    session_id: str
+    error: Optional[GradeCodeError] = None
+    test_results: Optional[TestRunnerResult] = None
